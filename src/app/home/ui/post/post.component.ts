5a99463e022679dcb00dfd8646b2551f41af6012
nikolasalim <nikolasalim@gmail.com>
2023-12-30 18:56:21 +0100

    implement logic for resetting Post state once it is inactive; display both key and value of each Post property

--- src/app/home/ui/post/post.component.ts
+++ src/app/home/ui/post/post.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 import { IPost } from "../../../shared/interfaces/post";
 
 @Component({
@@ -8,17 +16,42 @@ import { IPost } from "../../../shared/interfaces/post";
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
-export class PostComponent {
+export class PostComponent implements OnInit, OnChanges {
 
   @Input({required: true}) post!: IPost;
+  @Input() isActive: boolean = false;
   @Output() setCurrentPost: EventEmitter<void> = new EventEmitter<void>();
+  postInfoOrder: string[] = ['title', 'userId', 'id', 'body'];
+  defaultInfoOrder: string[] = [...this.postInfoOrder];
+  titleOnDisplay!: string;
+  valueOnDisplay!: string | number;
 
-  displayOrder = ['title', 'userId', 'id', 'body'];
+  ngOnInit() {
+    this.titleOnDisplay = this.postInfoOrder[0];
+    this.valueOnDisplay = this.post[this.postInfoOrder[0] as keyof IPost];
+  }
 
-  togglePostInfo(e: Event) {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes && changes['isActive']) {
+      if(changes['isActive'].previousValue && !changes['isActive'].currentValue) {
+        this.setDefaultInfoOrder();
+      }
+    }
+  }
+
+  togglePostInfo(e: MouseEvent) {
     e.preventDefault();
-    const shifted: string = this.displayOrder.shift()!;
-    this.displayOrder.push(shifted);
+    const shifted: string = this.postInfoOrder.shift()!;
+    this.postInfoOrder.push(shifted);
+    const keyToDisplay = this.postInfoOrder[0];
+    this.titleOnDisplay = keyToDisplay;
+    this.valueOnDisplay = this.post[keyToDisplay as keyof IPost];
     this.setCurrentPost.emit();
+  }
+
+  setDefaultInfoOrder(){
+    this.postInfoOrder = [...this.defaultInfoOrder];
+    this.titleOnDisplay = this.postInfoOrder[0];
+    this.valueOnDisplay = this.post[this.postInfoOrder[0] as keyof IPost];
   }
 }
