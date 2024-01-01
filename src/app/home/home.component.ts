84c7d719e5f593acd6f598cae274df44bb195f72
nikolasalim <nikolasalim@gmail.com>
2024-01-01 20:11:22 +0100

    fix approach for displaying 'no posts yet' indicator; tidy up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { PostsService } from "../shared/data-access/posts.service";
 import { CommonModule } from "@angular/common";
 import { PostComponent } from "./ui/post/post.component";
-import { IPost } from "../shared/interfaces/post";
 
 @Component({
   selector: 'app-home',
@@ -14,9 +13,5 @@ import { IPost } from "../shared/interfaces/post";
 export class HomeComponent {
 
   postsService = inject(PostsService);
-
-  setCurrentPost(post: IPost){
-    this.postsService.setCurrentPost(post);
-  }
 
 }
